821a5c847149ccc5469303d951ab0180149db6c8
Samuel <samuelbird01@gmail.com>
2025-03-18 21:40:38 -0600

    Wrote basic functionality for the looping menu and some of the functions the menu will perform

--- client/src/main/java/ui/Client.java
+++ client/src/main/java/ui/Client.java
@@ -1,4 +1,65 @@
 package ui;
+import java.util.Scanner;
 
 public class Client {
+    boolean isLoggedIn = false;
+
+    public void preLogin() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Welcome to CS 240 chess! ♛ ♚ ♞ \n Type 'help' to get started");
+            String input = scanner.nextLine();
+            switch (input) {
+                case "help":
+                    help();
+                case "quit":
+                    System.out.print("Are you sure you want to quit? y/n");
+                    input = scanner.next();
+                    if (input.equals("y")) {
+                        System.exit(0);
+                    }
+                case "register":
+                    register();
+                case "login":
+                    login();
+                    break;
+                default:
+                    System.out.println("Invalid input! Please type 'help' to get started");
+            }
+        }
+    }
+
+    private void help() {
+        System.out.println("""
+                Please type one of the following!
+                register -- to create an account
+                login -- to login to an existing account
+                quit -- to quit playing chess and exit the program
+                help -- lists possible commands""");
+    }
+
+    private void login() {
+        Scanner scanner = new Scanner(System.in);
+        String[] words;
+        System.out.println("Please enter your username and password like this: exampleUser examplePassword");
+        while (true) {
+            while (true) {
+                String loginCredentials = scanner.nextLine();
+                words = loginCredentials.split("\\s+");
+                if (words.length == 2) {
+                    break;
+                }
+                System.out.println("Error: cannot read input. Please enter your username and password with a single space in between and without a comma, like this: exampleUser examplePassword");
+            }
+            // make a call to the login api endpoint using words[0], words[1] and return anything if needed (phase 2)
+        }
+    }
+
+    private void register() {
+
+    }
+
+    private void btsLogin(String username, String password) {
+
+    }
 }
--- client/src/main/java/ui/DrawingBoard.java
+++ client/src/main/java/ui/DrawingBoard.java
@@ -3,7 +3,7 @@ package ui;
 public class DrawingBoard {
     String lightColor = EscapeSequences.SET_BG_COLOR_LIGHT_GREY + EscapeSequences.EMPTY;
     String darkColor = EscapeSequences.SET_BG_COLOR_DARK_GREY + EscapeSequences.EMPTY;
-    String borderColor = EscapeSequences.SET_BG_COLOR_BLUE + EscapeSequences.EMPTY;
+    String borderColor = EscapeSequences.SET_BG_COLOR_BLACK + EscapeSequences.EMPTY;
     String reset = EscapeSequences.RESET_BG_COLOR;
 
     String[][] board = {
